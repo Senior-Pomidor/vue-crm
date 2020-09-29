6cba0d0bdaa9aab32346320d6c95f14d20f89999
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-09-29 14:51:22 +0300

    Добавлен лоадер для MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,24 @@
 <template>
-<!-- лэйаут для основных страниц -->
-	<div class="app-main-layout">
-		
-		<Navbar @click="isOpen = !isOpen" />
+	<div>
+		<loader v-if="loading" />
+		<!-- лэйаут для основных страниц -->
+		<div class="app-main-layout" v-else>
+			
+			<Navbar @click="isOpen = !isOpen" />
 
-		<Sidebar v-model="isOpen" />
+			<Sidebar v-model="isOpen" />
 
-		<main class="app-content" :class="{full: !isOpen}">
-			<div class="app-page">
-				<router-view />
+			<main class="app-content" :class="{full: !isOpen}">
+				<div class="app-page">
+					<router-view />
+				</div>
+			</main>
+
+			<div class="fixed-action-btn">
+				<router-link class="btn-floating btn-large blue" to="/record">
+					<i class="large material-icons">add</i>
+				</router-link>
 			</div>
-		</main>
-
-		<div class="fixed-action-btn">
-			<router-link class="btn-floating btn-large blue" to="/record">
-				<i class="large material-icons">add</i>
-			</router-link>
 		</div>
 	</div>
 </template>
@@ -31,12 +34,15 @@ export default {
 		Sidebar
 	},
 	data: () => ({
-		isOpen: true
+		isOpen: true,
+		loading: true
 	}),
 	async mounted() {
 		if (!Object.keys(this.$store.getters.info).length) {
 			await this.$store.dispatch('fetchInfo')
 		}
+
+		this.loading = false
 	}
 }
 </script>
