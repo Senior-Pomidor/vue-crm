5c9d23fc6366a42401c48fb8a6551809434ffdcc
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-09-29 14:48:03 +0300

    Реализована кнопка рефреша на главной странице Home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 		<div class="page-title">
 			<h3>Счет</h3>
 
-			<button class="btn waves-effect waves-light btn-small">
+			<button class="btn waves-effect waves-light btn-small" @click="refresh">
 				<i class="material-icons">refresh</i>
 			</button>
 		</div>
@@ -24,6 +24,10 @@ import HomeCurrency from '@/components/HomeCurrency.vue'
 
 export default {
 	name: 'home',
+	components: {
+		HomeBill,
+		HomeCurrency
+	},
 	data: () => ({
 		loading: true,
 		currency: null
@@ -33,9 +37,12 @@ export default {
 		console.log(this.currency)
 		this.loading = false
 	},
-	components: {
-		HomeBill,
-		HomeCurrency
+	methods: {
+		async refresh() {
+			this.loading = true
+			this.currency = await this.$store.dispatch('fetchCurrency')
+			this.loading = false
+		}
 	}
 }
 </script>
